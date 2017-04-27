3ab07a71850d7f49ba6ef1b95a32147886aca7c0
Roberto Guido <bob4job@gmail.com>
2017-04-27 23:08:54 +0200

    rimosso lorem ipsum in pagina post-donazione

--- resources/views/donation/thanks.blade.php
+++ resources/views/donation/thanks.blade.php
@@ -6,10 +6,6 @@
     <div class="row primary-1">
         <div class="col-md-12">
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sodales, mauris vitae tristique laoreet, massa libero ultrices sem, eget ultricies nibh augue a nibh. Praesent eu vulputate enim. Donec volutpat eget nisl quis posuere. Praesent dapibus quam et arcu mattis hendrerit. Aliquam mattis quis nulla non viverra. Quisque id felis eu orci efficitur vehicula tincidunt ut elit. Donec magna mi, aliquet vitae malesuada sit amet, tempor nec quam. Morbi ultrices feugiat tellus at mattis. Nunc et viverra neque. Curabitur efficitur condimentum euismod. Vestibulum vel nunc ipsum.
-            </p>
-
-            <p>
                 <a class="dense-button" href="{{ url('/') }}">
                     <span>Torna alla homepage</span>
                 </a>
