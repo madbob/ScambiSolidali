2cbab656bff71629f06eee36baa3e4830dc78c1d
Roberto Guido <bob4job@gmail.com>
2017-07-03 23:59:55 +0200

    informazioni sulla donazione in scadenza nella mail di notifica

--- resources/views/mails/donationexpiring.blade.php
+++ resources/views/mails/donationexpiring.blade.php
@@ -1,5 +1,5 @@
 <p>
-    La tua donazione su {{ env('APP_NAME') }} purtroppo non è ancora stata assegnata e sta per scadere.
+    La tua donazione su {{ env('APP_NAME') }} ("{{ $donation->title }}" del {{ date('d/m/y', strtotime($donation->created_at)) }}) purtroppo non è ancora stata assegnata e sta per scadere.
 </p>
 <p>
     <a href="{!! url('celo/renew/' . urlencode(base64_encode(sprintf('%d-%d', $donation->id, $donation->user->id)))) !!}">Clicca qui</a> per rinnovare l'annuncio di un altro mese, oppure tra una settimana sarà automaticamente rimosso.
