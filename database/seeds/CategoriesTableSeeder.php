35cd1329032d62e2e5bdf8a5e9af4e3f742f440b
Roberto Guido <bob4job@gmail.com>
2017-10-19 16:10:34 +0200

    categoria "biancheria"

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -16,6 +16,7 @@ class CategoriesTableSeeder extends Seeder
             ],
             'Casa' => [
                 'Arredamento',
+                'Biancheria',
                 'Elettrodomestici',
                 'Elettronica',
                 'Utensili'
