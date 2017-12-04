96b0cf3c96e5f11ea7bfe72990020d0682411a74
Roberto Guido <bob4job@gmail.com>
2017-12-04 19:44:56 +0100

    tracking piwik

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -140,5 +140,22 @@
     @else
         <script src="{{ url('js/mine.js') }}"></script>
     @endif
+
+    <!-- Piwik -->
+    <script type="text/javascript">
+      var _paq = _paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['disableCookies']);
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//stats.madbob.org/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', '6']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <!-- End Piwik Code -->
 </body>
 </html>
