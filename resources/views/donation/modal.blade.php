a745fe003ffe26a66450bc76fa15c852cd395409
Roberto Guido <bob4job@gmail.com>
2018-05-17 17:42:45 +0200

    link diretto per donazioni e annunci

--- resources/views/donation/modal.blade.php
+++ resources/views/donation/modal.blade.php
@@ -6,6 +6,7 @@
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title" id="myModalLabel">{{ $donation->category->name }} | {{ $donation->title }}</h4>
+                <p class="text-muted"><small><br>{{ url('/celo?show=' . $donation->id) }}</small></p>
             </div>
             <div class="modal-body">
                 <div class="row">
