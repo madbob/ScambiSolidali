e2c405aa86f1718b226b2e1ecc858df71fd0fb23
Roberto Guido <bob4job@gmail.com>
2018-10-30 15:09:04 +0100

    aggiornamento testo descrizione progetto

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -25,18 +25,21 @@
                 <div class="row">
                     <div class="col-md-6 right-p">
                         <p>
-                            Il progetto prevede la realizzazione e sperimentazione di un sistema di reperimento e distribuzione di beni di prossimità di varia natura, basato su una rete locale di enti no profit e servizi di prossimità e su una piattaforma informatica che renda possibile l’incrocio della domanda/offerta di beni e servizi di prima necessità, riducendo al minimo l’impegno e i costi di natura logistica, in particolare per quanto concerne  lo stoccaggio, l’immagazzinamento e la distribuzione centralizzata.
+                            Il progetto prevede un sistema di reperimento e distribuzione di beni di prossimità di varia natura, basato su una rete locale di enti no profit e su una piattaforma informatica che renda possibile l'incrocio della domanda/offerta di beni e servizi di prima necessità, riducendo al minimo l'impegno e i costi di natura logistica, in particolare per quanto concerne lo stoccaggio, l'immagazzinamento e la distribuzione centralizzata.
                         </p>
                         <p>
-                            I beneficiari diretti delle donazioni saranno persone e famiglie svantaggiate, sia in condizione di marginalità cronica, sia in condizione di povertà grigia derivante da eventi traumatici anche recenti, come la perdita di lavoro, la separazione, la malattia.
+                            I beneficiari diretti delle donazioni sono persone e famiglie svantaggiate, sia in condizione di marginalità cronica, sia in condizione di povertà grigia derivante da eventi traumatici anche recenti, come la perdita di lavoro, la separazione, la malattia.
                         </p>
                     </div>
                     <div class="col-md-6 left-p">
                         <p>
-                            Sulla piattaforma i cittadini potranno donare beni materiali, i commercianti potranno donare fondi di magazzino o altri beni in eccesso, i professionisti potranno offrire gratuitamente servizi nei settori della salute e dell’abitare, le associazioni culturali potranno offrire accessi gratuiti a corsi, spettacoli e laboratori.
+                            Sulla piattaforma tutti possono donare beni materiali, i commercianti possono donare fondi di magazzino o altri beni in eccesso, i professionisti possono offrire gratuitamente servizi nei settori della salute e dell'abitare, le associazioni culturali possono offrire accessi gratuiti a corsi, spettacoli e laboratori.
                         </p>
                         <p>
-                            Gli  operatori accreditati ad accedere alla piattaforma fanno parte di una rete di organizzazioni che operano a contatto con persone e famiglie in difficoltà. Si tratta sia di organizzazioni che gestiscono servizi e progetti in ambito socio assistenziale, sia organizzazioni che, pur non avendo una mission esplicitamente sociale, entrano spesso in contatto con persone e famiglie in difficoltà. Saranno accreditati all’uso della piattaforma anche operatori dei servizi sociali pubblici.
+                            Gli operatori accreditati ad accedere alla piattaforma fanno parte di una rete di organizzazioni che operano a contatto con persone e famiglie in difficoltà. Si tratta sia di organizzazioni che gestiscono servizi e progetti in ambito socio assistenziale, sia organizzazioni che, pur non avendo una mission esplicitamente sociale, entrano spesso in contatto con persone e famiglie in difficoltà. Sono accreditati all’uso della piattaforma anche operatori dei servizi sociali pubblici.
+                        </p>
+                        <p>
+                            Per maggiori dettagli sul funzionamento visita <a href="/come-funziona">questa pagina</a>.
                         </p>
                     </div>
                 </div>
