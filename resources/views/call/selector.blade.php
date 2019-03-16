7649719a62edbac7e4c4f778aa47221929f49b5b
Roberto Guido <bob4job@gmail.com>
2019-03-16 12:17:46 +0100

    sistemato anche selettore di appelli in creazione donazione

--- resources/views/call/selector.blade.php
+++ resources/views/call/selector.blade.php
@@ -5,18 +5,16 @@
     <p>Seleziona, eventualmente, l'appello a cui stai rispondendo con questa donazione:</p>
 
     <div class="radio">
-        <label>
-            <input type="radio" name="call_id" value="-1" {{ $call == null ? 'checked' : '' }}>
-            Nessun appello specifico
-        </label>
+        <div class="radio radio-custom">
+            <input id="no_call" type="radio" name="call_id" value="-1" {{ $call == null ? 'checked' : '' }}>
+            <label for="no_call">Nessun appello specifico</label>
+        </div>
     </div>
 
     @foreach($call_query->get() as $iter_call)
-        <div class="radio">
-            <label>
-                <input type="radio" name="call_id" value="{{ $iter_call->id }}" {{ $call && $call->id == $iter_call->id ? 'checked' : '' }}>
-                {{ $iter_call->title }} <a class="show-details" data-endpoint="manca" data-item-id="{{ $iter_call->id }}">(dettagli)</a>
-            </label>
+        <div class="radio radio-custom">
+            <input id="call_{{ $iter_call->id }}" type="radio" name="call_id" value="{{ $iter_call->id }}" {{ $call && $call->id == $iter_call->id ? 'checked' : '' }}>
+            <label for="call_{{ $iter_call->id }}">{{ $iter_call->title }} <a class="show-details" data-endpoint="manca" data-item-id="{{ $iter_call->id }}">(dettagli)</a></label>
         </div>
     @endforeach
 
