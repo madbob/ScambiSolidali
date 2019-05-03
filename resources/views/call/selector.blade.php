bc5fc0e6d5bf958b15283c407a13dac6488a70f1
Roberto Guido <bob4job@gmail.com>
2019-05-03 15:48:49 +0200

    fix selezione appello in creazione donazione

--- resources/views/call/selector.blade.php
+++ resources/views/call/selector.blade.php
@@ -11,12 +11,22 @@
         </div>
     </div>
 
+    <?php $call_found = false ?>
+
     @foreach($call_query->get() as $iter_call)
+        <?php $call_found = ($call && $call->id == $iter_call->id) ?>
         <div class="radio radio-custom">
             <input id="call_{{ $iter_call->id }}" type="radio" name="call_id" value="{{ $iter_call->id }}" {{ $call && $call->id == $iter_call->id ? 'checked' : '' }}>
             <label for="call_{{ $iter_call->id }}">{{ $iter_call->title }} <a class="show-details" data-endpoint="manca" data-item-id="{{ $iter_call->id }}">(dettagli)</a></label>
         </div>
     @endforeach
+
+    @if(!$call_found && $call)
+        <div class="radio radio-custom">
+            <input id="call_{{ $call->id }}" type="radio" name="call_id" value="{{ $call->id }}" checked>
+            <label for="call_{{ $call->id }}">{{ $call->title }} <a class="show-details" data-endpoint="manca" data-item-id="{{ $call->id }}">(dettagli)</a></label>
+        </div>
+    @endif
 
     <p>
         Puoi consultare l'elenco completo degli appelli, con i relativi dettagli, su <a href="{{ url('manca') }}">questa pagina</a>.
