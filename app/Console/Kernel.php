8e34334dc510ae4eb7ab100bac8391dfd0e0658c
Roberto Guido <bob4job@gmail.com>
2019-10-08 12:06:55 +0200

    posticipato orario invio SMS notifica donazioni periodiche

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('check:expired')->daily();
-        $schedule->command('recurring:weekly')->weeklyOn(6, '11:00');
+        $schedule->command('recurring:weekly')->weeklyOn(6, '12:00');
     }
 
     protected function commands()
