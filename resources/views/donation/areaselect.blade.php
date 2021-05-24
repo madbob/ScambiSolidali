c78d4d5771b80721ccdec90aca4e0699a742bc3a
Roberto Guido <bob4job@gmail.com>
2021-05-24 11:09:05 +0200

    fix stupido selezione aree trentino

--- resources/views/donation/areaselect.blade.php
+++ resources/views/donation/areaselect.blade.php
@@ -52,32 +52,28 @@
     </div>
 @elseif($current_instance == 'Trentino')
     <div class="col-sm-4">
-        @for($i = 1; $i < 6; $i++)
-            <div class="checkbox">
-                <label>
-                    <input type="radio" name="receiver-area" value="COMUNITA' TERRITORIALE DELLA VAL DI FIEMME" required> COMUNITA' TERRITORIALE DELLA VAL DI FIEMME
-                    <input type="radio" name="receiver-area" value="COMUNITA' DI PRIMIERO" required> COMUNITA' DI PRIMIERO
-                    <input type="radio" name="receiver-area" value="COMUNITA' VALSUGANA E TESINO" required> COMUNITA' VALSUGANA E TESINO
-                    <input type="radio" name="receiver-area" value="COMUNITA' ALTA VALSUGANA E BERSNTOL" required> COMUNITA' ALTA VALSUGANA E BERSNTOL
-                    <input type="radio" name="receiver-area" value="COMUNITA' DELLA VALLE DI CEMBRA" required> COMUNITA' DELLA VALLE DI CEMBRA
-                    <input type="radio" name="receiver-area" value="COMUNITA' DELLA VAL DI NON" required> COMUNITA' DELLA VAL DI NON
-                </label>
-            </div>
-        @endfor
+        <div class="checkbox">
+            <label>
+                <input type="radio" name="receiver-area" value="COMUNITA' TERRITORIALE DELLA VAL DI FIEMME" required> COMUNITA' TERRITORIALE DELLA VAL DI FIEMME
+                <input type="radio" name="receiver-area" value="COMUNITA' DI PRIMIERO" required> COMUNITA' DI PRIMIERO
+                <input type="radio" name="receiver-area" value="COMUNITA' VALSUGANA E TESINO" required> COMUNITA' VALSUGANA E TESINO
+                <input type="radio" name="receiver-area" value="COMUNITA' ALTA VALSUGANA E BERSNTOL" required> COMUNITA' ALTA VALSUGANA E BERSNTOL
+                <input type="radio" name="receiver-area" value="COMUNITA' DELLA VALLE DI CEMBRA" required> COMUNITA' DELLA VALLE DI CEMBRA
+                <input type="radio" name="receiver-area" value="COMUNITA' DELLA VAL DI NON" required> COMUNITA' DELLA VAL DI NON
+            </label>
+        </div>
     </div>
     <div class="col-sm-4">
-        @for(; $i < 10; $i++)
-            <div class="checkbox">
-                <label>
-                    <input type="radio" name="receiver-area" value="COMUNITA' DELLA VALLE DI SOLE" required> COMUNITA' DELLA VALLE DI SOLE
-                    <input type="radio" name="receiver-area" value="COMUNITA' DELLE GIUDICARIE" required> COMUNITA' DELLE GIUDICARIE
-                    <input type="radio" name="receiver-area" value="COMUNITA' ALTO GARDA E LEDRO" required> COMUNITA' ALTO GARDA E LEDRO
-                    <input type="radio" name="receiver-area" value="COMUNITA' DELLA VALLAGARINA" required> COMUNITA' DELLA VALLAGARINA
-                    <input type="radio" name="receiver-area" value="COMUN GENERAL DE FASCIA" required> COMUN GENERAL DE FASCIA
-                    <input type="radio" name="receiver-area" value="MAGNIFICA COMUNITA' DEGLI ALTIPIANI CIMBRI" required> MAGNIFICA COMUNITA' DEGLI ALTIPIANI CIMBRI
-                </label>
-            </div>
-        @endfor
+        <div class="checkbox">
+            <label>
+                <input type="radio" name="receiver-area" value="COMUNITA' DELLA VALLE DI SOLE" required> COMUNITA' DELLA VALLE DI SOLE
+                <input type="radio" name="receiver-area" value="COMUNITA' DELLE GIUDICARIE" required> COMUNITA' DELLE GIUDICARIE
+                <input type="radio" name="receiver-area" value="COMUNITA' ALTO GARDA E LEDRO" required> COMUNITA' ALTO GARDA E LEDRO
+                <input type="radio" name="receiver-area" value="COMUNITA' DELLA VALLAGARINA" required> COMUNITA' DELLA VALLAGARINA
+                <input type="radio" name="receiver-area" value="COMUN GENERAL DE FASCIA" required> COMUN GENERAL DE FASCIA
+                <input type="radio" name="receiver-area" value="MAGNIFICA COMUNITA' DEGLI ALTIPIANI CIMBRI" required> MAGNIFICA COMUNITA' DEGLI ALTIPIANI CIMBRI
+            </label>
+        </div>
         <div class="checkbox">
             <label>
                 <input type="radio" name="receiver-area" value="COMUNITA' ROTALIANA - KONIGSBERG" required> COMUNITA' ROTALIANA - KONIGSBERG
