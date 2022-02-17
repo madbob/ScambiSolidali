0e7686d3a15d50e218f8a660174ed20723ee197a
Roberto Guido <info@madbob.org>
2022-02-17 09:49:06 +0100

    revisione testi per istanza Torino

--- database/seeds/TorinoTableSeeder.php
+++ database/seeds/TorinoTableSeeder.php
@@ -215,18 +215,15 @@ class TorinoTableSeeder extends Seeder
                         <li>il donatore è disponibile a consegnare il bene al beneficiario dopo aver preso accordi con l’Ente accreditato di riferimento</li>
                         <li>l’Ente accreditato provvede al ritiro e alla consegna del bene</li>
                         <li>il beneficiario provvede in modo autonomo al ritiro del bene previ accordi tra donatore e l’Ente accreditato</li>
-                        <li>il beneficiario o l’Ente accreditato sono in grado di sostenere la spesa di trasporto e possono chiedere un preventivo a un fornitore di loro fiducia oppure alla Cooperativa La Fonte, partner del nostro progetto (sotto i dettagli)</li>
-                        <li>nel caso di impossibilità ad effettuare il trasporto da parte dell’Ente accreditato o in caso  di gravi difficoltà economiche del beneficiario, il progetto celocelo può contribuire a sostenere le spese del trasporto richiesto, l’operatore dovrà richiedere il trasporto quando assegna il bene tramite il sito</li>
+                        <li>il beneficiario o l’Ente accreditato sono in grado di sostenere la spesa di trasporto e possono chiedere un preventivo a un fornitore di loro fiducia oppure contattarci via mail per richiedere indicazioni in merito</li>
+                        <li>nel caso di impossibilità ad effettuare il trasporto da parte dell’Ente accreditato o in caso di gravi difficoltà economiche del beneficiario, il progetto celocelo può valutare se in grado di contribuire a sostenere parte delle spese del trasporto richiesto, l’operatore dovrà richiedere il trasporto quando assegna il bene tramite il sito e compilare il form con i dati</li>
                     </ul>
 
                     <h4>Smontaggio</h4>
 
                     <p>Il progetto non sostiene le spese di smontaggio e montaggio.</p>
                     <p>Chiediamo al donatore di far trovare i mobili smontati e a chi riceve di provvedere al montaggio.</p>
-                    <p>Nel caso di impossibilità a provvedere allo smontaggio e al montaggio di seguito trovate i contatti e le tariffe dei nostri fornitori di fiducia (in caso di gravi difficoltà il progetto celocelo può contribuire a sostenerne le spese).</p>
-                    <p>La Fonte Solidale Cooperativa Sociale<br>corso Vinzaglio 2, Torino</p>
-                    <p>Per informazioni  e preventivi : lafonte.solidale@gmail.com<br>Tel. 351 2276924, Enzo Covito</p>
-                    <p>Tariffe:<br>17 euro all\'ora a persona per la manodopera<br>90 centesimi al km per il trasporto</p>
+                    <p>Nel caso di impossibilità a provvedere allo smontaggio e al montaggio potrete contattarci e potremo darvi indicazioni per richiedere un preventivo.</p>
                 </div>
             </div>';
 
