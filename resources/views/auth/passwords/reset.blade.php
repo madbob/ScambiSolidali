935bc1e1d48ed94f215122f68d2e8d28f172b038
Roberto Guido <info@madbob.org>
2025-01-23 10:13:24 +0100

    fix pannello cambio password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -4,7 +4,7 @@
 
 <div class="row justify-content-center my-5">
     <div class="col-md-6 primary-1">
-        <x-larastrap::form route="password.update" :button="[['element' => 'larastrap::sbtn', 'label' => 'Resetta Password']]">
+        <x-larastrap::form route="password.update" :buttons="[['element' => 'larastrap::sbtn', 'label' => 'Resetta Password']]">
             <x-larastrap::hidden name="token" :value="$token" />
             <x-larastrap::email name="email" label="E-Mail" required />
             <x-larastrap::password name="password" label="Password" required />
