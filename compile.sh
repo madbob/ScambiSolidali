c1ff7fb763793c3ae39c5c3c743080e330284449
Roberto Guido <info@madbob.org>
2025-10-02 14:47:43 +0200

    aggiornato regolamento per trentino

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sassc resources/assets/sass/torino.scss public/css/torino.css
-sassc resources/assets/sass/milano.scss public/css/milano.css
-sassc resources/assets/sass/valsusa.scss public/css/valsusa.css
-sassc resources/assets/sass/trentino.scss public/css/trentino.css
+sass resources/assets/sass/torino.scss public/css/torino.css
+sass resources/assets/sass/milano.scss public/css/milano.css
+sass resources/assets/sass/valsusa.scss public/css/valsusa.css
+sass resources/assets/sass/trentino.scss public/css/trentino.css
 rm -f public/css/builds/*
