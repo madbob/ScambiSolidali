fc35cefff90806cde2c1d782a0da4c6423bfa59c
Roberto Guido <info@madbob.org>
2026-02-26 10:16:54 +0100

    stupid fix for renew emails

--- resources/views/mails/donationexpiring.blade.php
+++ resources/views/mails/donationexpiring.blade.php
@@ -2,5 +2,5 @@
     La tua donazione su {{ env('APP_NAME') }} ("{{ $donation->title }}" del {{ date('d/m/y', strtotime($donation->created_at)) }}) purtroppo non è ancora stata assegnata e sta per scadere.
 </p>
 <p>
-    <a href="{{ route('celo.renew', ['token' => $donation->renewToken()]) !!}">Clicca qui</a> per rinnovare l'annuncio di altri due mesi, oppure tra una settimana sarà automaticamente rimosso.
+    <a href="{!! route('celo.renew', ['token' => $donation->renewToken()]) !!}">Clicca qui</a> per rinnovare l'annuncio di altri due mesi, oppure tra una settimana sarà automaticamente rimosso.
 </p>
